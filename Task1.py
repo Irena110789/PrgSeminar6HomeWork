6cb6774f2c73727ec287ac08abae0b530cd7d27f
Andrey210589 <PolishhukAk@mail.ru>
2023-06-09 09:51:55 +0300

    Init_commit

---
+++ Task1.py
@@ -0,0 +1,22 @@
+# Задача 30:  
+# Заполните массив элементами арифметической прогрессии. 
+# Её первый элемент, 
+# разность и количество элементов нужно ввести с клавиатуры. 
+# Формула для получения n-го члена прогрессии: 
+# an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+# Ввод: 7 2 5
+# Вывод: 7 9 11 13 15
+
+def arifmethic(n,end,step):
+    arrey=[]
+    for i in range(n,n+(end-1)*step+1,step):
+        arrey.append(i)
+    return arrey
+   
+n = int(input("First element "))
+step = int(input("Step "))
+end = int(input("Elements emmount  "))
+
+print(arifmethic(n,end,step))
+
